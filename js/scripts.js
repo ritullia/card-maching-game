fdfa95082f89e870cc13e4674ec522235cc6492c
ritullia <brigrita@gmail.com>
2023-02-22 21:06:56 +0200

    flip card js commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log("veikia")
+console.log("veikia");
+
+const cards = document.querySelectorAll('.card');
+console.log(cards);
+
+function flipCard(e) {
+    let clickCard = e.target;
+    clickCard.classList.add('flip')
+    console.log(clickCard)
+
+}
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+})
