37927b3cda35b750db36c145fce5877b8d8186ee
Rita <94860684+ritullia@users.noreply.github.com>
2023-02-23 13:22:19 +0200

    Dev (#2)
    * cards style
    
    * flip card js commit
    
    * flip card js
    
    * something not ok
    
    * js flip-card commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
-console.log("veikia")
+console.log("veikia");
+
+const cards = document.querySelectorAll('.cards .card');
+console.log(cards);
+
+let counter = 0;
+let firstClick;
+let secondClick;
+
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        card.classList.add('flipped');
+
+        console.log(card);
+
+        if (counter === 0) {
+            firstClick = card.getAttribute("emoji")
+            counter++;
+        } else {
+            secondClick = card.getAttribute("emoji")
+            counter = 0
+
+            if (firstClick === secondClick) {
+                const correctCard = document.querySelectorAll(".card[emoji='" + firstClick + "'] ")
+
+                correctCard[0].classList.add('flip-card')
+                correctCard[1].classList.add('flip-card')
+            }
+
+
+        }
+
+
+
+        console.log(firstClick)
+        console.log(secondClick)
+    })
+})
+
