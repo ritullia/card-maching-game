72e78bc926f14bf898b6da25c9d3bd2dbb862f79
ritullia <brigrita@gmail.com>
2023-02-23 21:39:29 +0200

    matched cards commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,43 @@
 console.log("veikia");
 
-const cards = document.querySelectorAll('.cards .card');
+const cards = document.querySelectorAll(".cards .card");
 console.log(cards);
 
 //  paspaudimo veiksmas kiekvienai kortelei
 cards.forEach((card) => {
-    card.addEventListener('click', () => {
-        cardFlipped(card)
-        matchCards(card)
-
-    })
+  card.addEventListener("click", () => {
+    cardFlipped(card);
+    matchCards(card);
+  });
 });
-
 
 let firstCard;
 let secondCard;
 
 function cardFlipped(card) {
-    card.classList.add('flipped');
-
-
+  card.classList.add("flipped");
 }
-
 
 //funkcija sutampancioms kortelems
 function matchCards(card) {
-    let firstCard = card.imageObject.src
-    console.log(firstCard)
+  if (firstCard) {
+    secondCard = card;
+    if (firstCard.getAttribute("emoji") === secondCard.getAttribute("emoji")) {
+      console.log("Paveiksliukai sutampa");
+    } else {
+      setTimeout(() => {
+        secondCard.classList.remove("flipped");
+        firstCard.classList.remove("flipped");
+      }, 2000);
+    }
+    setTimeout(() => {
+      firstCard = null;
+    }, 2000);
+  } else {
+    firstCard = card;
+  }
 
-
+  console.log(firstCard);
 }
 
 const cardArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
